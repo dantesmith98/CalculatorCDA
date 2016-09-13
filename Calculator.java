810d7eb3cb9d0fc4cd25b18676e1272e8eb77c5d
dantesmith98 <dsmith2@utcmediacityuk.org.uk>
2016-09-13 12:41:46 +0100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+    add(5,6);
+    subtract(5,6);
+    
+    }
+    public static int add (int x, int y) {
+        int total = x + y;
+        System.out.println(total);
+        return total;
+    }
+         
+        public static int subtract (int x, int y) {
+        int total = x - y;
+        System.out.println(total);
+        return total;
+         
+      
+    }
+}
+        
+  
